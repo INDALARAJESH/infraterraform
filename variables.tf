ff8593d3c0a714453083aa8fd902fcf84342d76f
INDALARAJESH <109213968+INDALARAJESH@users.noreply.github.com>
2022-11-04 23:31:11 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "officelapkey" 
+    default     =  "key-pair" 
 }
 
 variable "instance_type" { 
@@ -24,7 +24,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-062df10d14676e201" 
+    default     = "ami-0c4292d03ccfa739c" 
 }
 variable "versioning" {
     type        = bool
