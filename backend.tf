ad3953f79d427a0d2c9cdfe30dc5a82f8b2ca7c6
INDALARAJESH <109213968+INDALARAJESH@users.noreply.github.com>
2022-11-06 11:45:45 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket-raj"
+    bucket = "mydev-tf-state-bucket-raj1"
     key = "main"
-    region = "ap-south-1"
-    dynamodb_table = "DynamoDB-raj"
+    region = "us-east-2"
+    dynamodb_table = "DynamoDB-raj1"
   }
 }
